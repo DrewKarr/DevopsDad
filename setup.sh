b3686cdb03ccc7d4a4ff8eb87df26d11fd43b815
Drew K <drewk@Drews-MacBook-Air.local>
2022-03-06 14:05:11 -0500

    fix setup script with permissions

--- setup.sh
+++ setup.sh
@@ -1,46 +1,42 @@
-# !/bin/bash# 
-
-export K8S_VERSION=1.22.6-gke.1000
-export PROJECT_ID=doc-drew-202203041740031
+#!/bin/bash
+set -v
+export PROJECT_ID=drewlearns-$(date +%Y%m%d%H%M%S)
 export TF_VAR_project_id=$PROJECT_ID
-export TF_VAR_state_bucket=$PROJECT_ID
-
-terraform init
-terraform apply --var k8s_version=$K8S_VERSION
-
+export SERVICE_ACCOUNT=drewlearns-$(date +%Y%m%d%H%M%S)
 export KUBECONFIG=$PWD/kubeconfig
 
-gcloud container clusters \
-    get-credentials \
-    $(terraform output --raw cluster_name) \
-    --project $(terraform output --raw project_id) \
-    --region $(terraform output --raw region)
-
-kubectl create clusterrolebinding \
-    cluster-admin-binding \
-    --clusterrole \
-    cluster-admin \
-    --user \
-    $(gcloud config get-value account)
-
+# Run everything from the IaC section
+[ $(helm version) ] || brew install helm
+gcloud auth login
+gcloud projects create $PROJECT_ID
+gcloud projects list | grep $PROJECT_ID
+gcloud iam service-accounts create $SERVICE_ACCOUNT --project $PROJECT_ID --display-name SERVICE_ACCOUNT
+gcloud iam service-accounts list --project $PROJECT_ID
+gcloud iam service-accounts keys create account.json --iam-account $SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
+gcloud iam service-accounts keys list --iam-account=$(gcloud iam service-accounts list --project $PROJECT_ID | grep -o "$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com") --project $PROJECT_ID --format=json
+gcloud projects add-iam-policy-binding $PROJECT_ID --member serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com --role roles/owner
+gsutil ls -p $PROJECT_ID 
+gcloud container get-server-config --region us-east1 --project $PROJECT_ID 
+gcloud container clusters get-credentials $(terraform output -raw cluster_name) \
+	--project $(terraform output -raw project_id)	\
+	--region $(terraform output -raw region)
 kubectl get nodes
+kubectl version --output yaml
+cat backend.tf k8s-control-plane.tf k8s-worker-nodes.tf provider.tf storage.tf | tee main.tf
+rm -f backend.tf k8s-control-plane.tf k8s-worker-nodes.tf provider.tf storage.tf
 
-cd ../..
-
-#############################
-# Deploy Ingress Controller #
-#############################
-
-kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.0/deploy/static/provider/cloud/deploy.yaml
-
-export INGRESS_HOST=$(kubectl \
+# Install ingress controller
+kubectl apply --filename ingress-nginx.yaml && \
+while [ -s $INGRESS_HOST ]; do
+    export INGRESS_HOST=$(kubectl \
     --namespace ingress-nginx \
     get svc ingress-nginx-controller \
     --output jsonpath="{.status.loadBalancer.ingress[0].ip}")
-export INGRESS_HOST=$(kubectl \
-    --namespace ingress-nginx \
-    get svc ingress-nginx-controller \
-    --output jsonpath="{.status.loadBalancer.ingress[0].ip}")
-
+done;
 echo $INGRESS_HOST
-kubectl get ingress {{ template "fullname" . }}
+
+# Create a template using helm
+helm create demo-app
+
+terraform init
+terraform apply
